75f3f974366fbe9d99328437ed055b21ed046341
sergioahp <sergioahp@proton.me>
2025-07-14 00:30:48 -0600

    fp-16-training

--- book/sae-plan-de-investigacion.py
+++ book/sae-plan-de-investigacion.py
@@ -85,6 +85,75 @@
 # $$
 #     6(2.1 \times 10^9)(2048^2)(8)(2) = 8.5 \times 10^{17}
 # $$
+#
+# the configuration foound to work with fp16 training the
+# best is
+# x ~ N(0, 64/d_model)
+# with d_sae either 2048*32 or 2048*24
+# W ~ N(0, 64/d_sae) that is N(0, 1/1024) (the matrix the encoder and
+# decoder have in common at initialization time)
+# we suspect this avoids underfows
+# what follows is a carefull analysis if the forwards and backwards graph, in an
+# attempt to align theory the practice
+#
+# The product of such entry of x and of W is has variance
+#
+# Var(x) Var(w) = 64/d_model * 64/d_sae aprox 0.01
+#
+# Then, we add d_model of those together, to get an that the preactivations have
+# variance:
+#
+# Var(x) d_model * 64/d_model 64/d_sae = 64**2/d_sae aprox 1/4
+#
+# then, if the l2^2 is between 0.4 and 0.02, and is a sum of
+# d_model elemets per thing in the batch, then each is about
+# 0.01 to 0.001 on average per element, thus it represents
+# no underflow risk
+
+# now let's think of the backwards pass:
+# the l0 loss: we warm it up from 0 to l0*0.0009 during training,
+# thus, on the decoder branch, we get all the full loss, which starts at
+# about d_sae/2, grows for some steps, then comes down rapidly
+# thus, first it gets spread over all d_model elemens, that is
+# d_sae/(2 d_model), thus about 12 or 16
+# then it gets mult by a special scalar designed to scale the gradient
+# contributions properly to stabilize training: 1/x_mult**2 so 1/64
+# then it gets scaled up by 2 (because of squaring)
+# so at this point each float is around 6/64 or 8/64
+# which is 0.9 to 0.12
+# so if we think of that as the std
+# then the variance is 0.009 to 0.01
+# finally, it passes the backward of whose forwards is
+# acts -> acts W^t, that is
+# grad_out -> grad_out @ self.dec.weight
+# at this point its variance is
+#
+# (0.009 to 0.012) * 64 / d_sae 
+#
+# thus, here we have an std of 0.006 to 0.007
+# confortably represented as a normal float16
+# once you have those multiplications, don't forget to sum!
+# each latent neuron gets contributions from d_model things, so we multiply our
+# variance above
+# current var: from 12  to 14 let's round to 13
+# then, we pass this grad throud the jumprelu, which
+# just passes the gradient or zeroes depending on the threshold set by the Step
+# function, at the begining of thraining roughly half of the gradients are
+# blocked thus after this we have a variance of of 7.5 still safe.
+# we follow by multiplying with the encoder matrix whose entries have variance
+# 64/d_sae, thus after this multiplication of floats we have
+# current var: 7.5 * 64/ d_sae
+# or a std of 0.2
+# then each float in the input to the encoder gets contributions from all the
+# ACTIVE latent neurons, do d_sae / 2
+# we sum all of those identical thinkgs to ger a variance
+# d_sae/2 * 7.5 * 64/d_sae = 7.5 * 32
+# or a std of 15.5, which is large but does not cause overflow in float16.
+# finally, we scale (up this time) by that special scalar, giving us a (without
+# squaring this time) (which is 8)
+# almost cancelling the 7.5 and resulting on a variance of 32
+# thus, we did avoid underflows and overflows.
+
 
 # # El modelo
 
@@ -111,7 +180,8 @@ import matplotlib.pyplot as plt
 # In[ ]:
 
 
-dtype = torch.float32
+dtype = torch.float16
+lower_dtype = torch.float16
 device = "cuda" if torch.cuda.is_available() else "cpu"
 USE_PROFILER = False
 
@@ -119,6 +189,8 @@ USE_PROFILER = False
 # In[ ]:
 
 
+norm_mult = 8.
+x_mult = 8.
 class Step(torch.autograd.Function):
     @staticmethod
     def forward(ctx, x, threshold):
@@ -127,7 +199,7 @@ class Step(torch.autograd.Function):
 
     @staticmethod
     def backward(ctx, grad_output):
-        bandwidth = 0.001
+        bandwidth = 0.001*norm_mult*x_mult
         x, threshold = ctx.saved_tensors
         mask = (abs(x - threshold) < bandwidth/2) & (x > 0)
         grad_threshold = -1.0/bandwidth * mask.to(x.dtype)
@@ -141,15 +213,16 @@ class Sae(nn.Module):
     def __init__(self, d_in, d_sae, use_pre_enc_bias=True, **kwargs):
         super().__init__(**kwargs)
         self.enc = nn.Linear(d_in, d_sae, dtype=dtype)
-        self.dec = nn.Linear(d_sae, d_in, dtype=dtype)
+        self.dec = nn.Linear(d_sae, d_in, dtype=lower_dtype)
         with torch.no_grad():
             # normalize each of the d_sae dictonary vectors
             self.dec.weight /= vector_norm(self.dec.weight, dim=1, keepdim=True)
-            self.enc.weight.copy_(self.dec.weight.clone().t())
+            self.dec.weight *= norm_mult
+            self.enc.weight.copy_(self.dec.weight.clone().t().to(self.enc.weight.dtype))
             self.enc.bias.copy_(torch.zeros_like(self.enc.bias))
             self.dec.bias.copy_(torch.zeros_like(self.dec.bias))
         self.log_threshold = nn.Parameter(
-            torch.log(torch.full((d_sae,), 0.001, dtype=dtype)))
+            torch.log(torch.full((d_sae,), 0.001 * norm_mult*x_mult, dtype=dtype)))
         self.use_pre_enc_bias = use_pre_enc_bias
         def project_out_parallel_grad(dim, tensor):
             @torch.no_grad
@@ -173,15 +246,24 @@ class Sae(nn.Module):
         d = {}
         original_input = x
         if self.use_pre_enc_bias:
-            x = x - self.dec.bias
+            x = x - self.dec.bias.to(x.dtype)
         
         x = self.enc(x)
         threshold = torch.exp(self.log_threshold)
         s = Step.apply(x, threshold)
         x = x*s
-        x = self.dec(x)
+        # print((x==0).sum())
+        # x *= 100.
+        x = x.to(lower_dtype)
+        # import sys;sys.exit() 
+        # x = self.dec(x)
+        x = x @ self.dec.weight.t()
+        x += self.dec.bias
+        x = x.to(original_input.dtype)
+        # x /= 100.
+        x /= norm_mult**2
         d['mask'] = s
-        d['reconstruction'] = ((x - original_input).pow(2)).mean(0).sum()
+        d['reconstruction'] = ((x - original_input).pow(2)).mean(0).sum()/x_mult**2
 
         return d
 
@@ -296,19 +378,19 @@ torch.set_float32_matmul_precision('high')
 steps = 2**16
 max_lr = 7e-5
 d_in = 2048
-d_sae = 2048*8
+d_sae = 2048*32
 model = Sae(d_in, d_sae)
 model.to(device)
 model.compile(
-#         mode="max-autotune",
-#         dynamic=False,
-#         fullgraph=True,
+        mode="max-autotune",
+        dynamic=False,
+        fullgraph=True,
 #         options = {
 #             "max_autotune":True,
 #             "epilogue_fusion":True,
 #             "triton.cudagraphs":True,
 #             },
-        )
+         )
 warmup_steps=1000
 sparcity_warmup_steps = 256000
 total_steps=256000 #for now
@@ -324,7 +406,7 @@ scheduler = torch.optim.lr_scheduler.LambdaLR(
     lr_lambda=lambda step: cosine_schedule_with_warmup(step, warmup_steps, total_steps)
 )
 
-writer = SummaryWriter("/workspace/runs/sae-good-config-new-heatmap")
+writer = SummaryWriter("/workspace/runs/allfp16x_mult8scale8warmup1000beta999v3x32compile")
 
 should_break = False
 total_step = 0
@@ -338,7 +420,8 @@ eval_prevalences_every = 500000
 
 bin_edges = np.logspace(np.log10(1e-8), np.log10(100), num=101).tolist()
 
-plot_features_every = 1000
+plot_features_every = 0
+plot_params_every = 5000
 
 def heatmap_feature_products(features):
     ...
@@ -363,6 +446,7 @@ with training_ctx:
     while total_step < total_steps:
         for step, x in enumerate(tqdm(dataloader)):
             x = x.to(device, non_blocking=True).to(dtype)
+            x *= x_mult
             x /= 3.4 # this is supposed to be the expected norm
             optimizer.zero_grad()
             # you can do without the prevalences, use a rolling average for
@@ -412,7 +496,7 @@ with training_ctx:
                         # Should we need to call .detach().cpu().item()?
                         writer.add_scalar("percent dead", percent_dead, total_step)
 
-                if total_step % plot_features_every == 0:
+                if (plot_features_every > 0) and (total_step % plot_features_every) == 0:
                     to_plot = 256
                     features = model.dec.weight
                     dot_products = features.T @ features
@@ -422,9 +506,13 @@ with training_ctx:
                     dot_products = dot_products[idx]
                     dot_products = dot_products[:, idx]
                     dot_products = dot_products / dot_products.max()
-                    dot_products = cmap(dot_products.detach().cpu().numpy())[...,:3]
+                    dot_products = cmap(dot_products.detach().float().cpu().numpy())[...,:3]
                     dot_products = torch.as_tensor(dot_products).permute(2 ,0 ,1)
                     writer.add_image("dot products", dot_products, total_step)
+
+                if total_step % plot_params_every == 0:
+                    writer.add_histogram("wdec", model.dec.weight, total_step)
+                    writer.add_histogram("wenc", model.enc.weight, total_step)
 
 
 
@@ -435,7 +523,6 @@ with training_ctx:
             loss = reconstruction_loss + sparsity_coefficient * l0
             # log losses, compute stats, etc
             grad = loss.backward()
-            # norm = torch.nn.utils.clip_grad_norm_(model.parameters(), 1.0)
             # metrics
             if (total_step % 5000) == 0:
                 with torch.no_grad():
@@ -459,6 +546,8 @@ with training_ctx:
                 writer.add_scalar("sparsity coefficient",
                         sparsity_coefficient,
                         total_step)
+            grad_norm = torch.nn.utils.clip_grad_norm_(model.parameters(), .9)
+            writer.add_scalar("grad_norm", grad_norm, total_step)
             optimizer.step()
             # TODO: sparsity_coefficient scheduler
             # print(scheduler.get_last_lr()[0])
@@ -466,8 +555,9 @@ with training_ctx:
 
             # normalize
             with torch.no_grad():
-                wdecnorm = vector_norm(model.dec.weight, dim=1, keepdim=True)
+                wdecnorm = vector_norm(model.dec.weight, dim=1, keepdim=True, dtype=dtype).to(lower_dtype)
                 model.dec.weight /= wdecnorm
+                model.dec.weight *= norm_mult
         # print(f"epoch loss: {loss.detach().item()}")
             total_step +=1
             if total_step > total_steps:
